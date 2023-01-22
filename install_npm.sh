d4f11b645d9413637431c9ee578710751b69930d
soc06212 <soc06212@gmail.com>
2023-01-22 22:32:22 +0900

    Doc: NPM 패키지 설치 스크립트 추가

---
+++ install_npm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+npm install express
+npm install fs
+npm install express-session
+npm install memorystore
+npm install ejs
+npm install body-parser
+npm install highlight.js
+npm install markdown-it
+npm install mysql2
+npm install path
+npm install multer
